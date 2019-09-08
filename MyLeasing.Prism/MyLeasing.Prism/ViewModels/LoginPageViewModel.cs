3af3ff0288870710c9e8bf371e0f9c2449a8bbf4
Jose Luis Enrique Sanchez Avalos <tundertundre@hotmail.com>
2019-09-08 01:56:12 -0400

    Login without service Part 28 Se la creacion de una pantalla basica de login con los binding necesarios para un controls general de validacion por mensajes

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,16 +1,63 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MyLeasing.Prism.ViewModels
 {
-    public class LoginPageViewModel : BindableBase
+    public class LoginPageViewModel : ViewModelBase
     {
-        public LoginPageViewModel()
-        {
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnable;
+        private DelegateCommand _loginCommand;
 
+        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Login";
+            IsEnable = true;
         }
+
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+        public string Email { get; set; }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnable
+        {
+            get => _isEnable;
+            set => SetProperty(ref _isEnable, value);
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Acept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an password.", "Acept");
+                return;
+            }
+
+            await App.Current.MainPage.DisplayAlert("OK", "No mames wey funciona.", "Acept");
+        }
+
     }
 }
