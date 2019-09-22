6a50404b0bb2424d902117231280ac54d3ea4026
Jose Luis Enrique Sanchez Avalos <tundertundre@hotmail.com>
2019-09-22 08:45:00 -0400

    Rest Password Part 49, the previous commit correspond to part 48 Reseteo de Clave, el commit previo corresponde a la parte 48

--- MyLeasing.Web/Helpers/IUserHelper.cs
+++ MyLeasing.Web/Helpers/IUserHelper.cs
@@ -37,5 +37,10 @@ namespace MyLeasing.Web.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
+
     }
 }
